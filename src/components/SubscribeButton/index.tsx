5911262e795d0027c6314c5449e07e41b18c4417
lucas <lucassantoscrfbezerra@gmail.com>
2021-06-20 11:15:05 -0300

    feat: concluindo checkout no fauna/stripe, envitando duplicaçao de customers

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,41 +1,39 @@
-import { signIn, useSession } from "next-auth/client";
-import { api } from "../../services/api";
-import { getStripeJs } from "../../services/stripe-js";
-import styles from "./styles.module.scss";
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss';
 
 type SubscribeButtonProps = {
-  priceId: string;
+    priceId: string;
 };
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  const[session] = useSession();
+    const [session] = useSession();
 
-  async function handleSubscribe(){
-    if(!session){
-      signIn('github');
-      return;
+    async function handleSubscribe() {
+        if (!session) {
+            signIn('github');
+            return;
+        }
+
+        //criação da checkout session no stripe
+        try {
+            const response = await api.post('/subscribe');
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+            await stripe.redirectToCheckout({ sessionId });
+        } catch (error) {
+            alert(error.message);
+        }
     }
 
-    //criação da checkout session no stripe
-    try {
-      const response = await api.post('/subscribe')
-      const { sessionId } = response.data;
-
-      const stripe = await getStripeJs()
-      await  stripe.redirectToCheckout({ sessionId })
-      
-    } catch (error) {
-      alert(error.message)
-    }
-
-  }
-
-  return (
-    <button
-       type="button" 
-       className={styles.subscribeButton}
-       onClick={handleSubscribe}
-    >
-      Subscribe now
-    </button>
-  );
+    return (
+        <button
+            type="button"
+            className={styles.subscribeButton}
+            onClick={handleSubscribe}
+        >
+            Subscribe now
+        </button>
+    );
 }
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,20 +1,66 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
+import { query as q } from "faunadb";
 import { stripe } from "../../services/stripe";
 
+type User = {
+  ref: {
+    id: string
+  }
+  data: {
+    stripe_customer_id: string
+  }
+}
+//tem que ser obrigatoriamente com estes nomes =>req e res
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   if (req.method === 'POST') {
     const session = await getSession({ req });
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-      //metadata:
-    })
+    /**
+     * Verifica se o cliente já existe no fauna retornando ele
+     * 
+     */
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
+
+
+    let customerId = user?.data?.stripe_customer_id
+
+    if (!customerId) {
+      //criando o custorme no stripe
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        //metadata:
+      })
+      /**
+       * Pegando aquele usuario que já existe e adicionando o customer_id a ele
+       */
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
+
+      customerId = stripeCustomer.id
+    }
+
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-
-      customer: stripeCustomer.id,
+      //teste
+      customer: customerId,
       //metodo de pagamento
       payment_method_types: ['card'],
       //endereço obrigatorio ou tratar no stripe
