d1d5bbe1f9a7ba22cbd4ee032fb233da863c6896
lucas <lucassantoscrfbezerra@gmail.com>
2021-08-03 16:11:18 -0300

    test: adicionando ultimo teste de caso de uso no component ActiveLink

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -12,18 +12,17 @@ jest.mock('next/router', () => {
 });
 
 describe('ActiveLink Component', () => {
-    test('active link renders correctly', () => {
+    test('Active link renders correctly', () => {
         const { getByText } = render(
             <ActiveLink href="/" activeClassName="active">
                 <a>Test</a>
             </ActiveLink>
         );
-
-        //espera pegar o texto Test contido no documento
+        //espera pegar o texto Test esteja contido no documento
         expect(getByText('Test')).toBeInTheDocument();
     });
 
-    test('active link is receiving active class', () => {
+    test('Active link is receiving active class', () => {
         const { getByText } = render(
             <ActiveLink href="/" activeClassName="active">
                 <a>Test</a>
@@ -31,5 +30,15 @@ describe('ActiveLink Component', () => {
         );
         //espera pegar o text o verificar se o component contem, a class active
         expect(getByText('Test')).toHaveClass('active');
+    });
+
+    test('Not Active link is receiving dont active class', () => {
+        const { getByText } = render(
+            <ActiveLink href="/teste" activeClassName="active">
+                <a>Test</a>
+            </ActiveLink>
+        );
+
+        expect(getByText('Test')).not.toHaveClass('active');
     });
 });
